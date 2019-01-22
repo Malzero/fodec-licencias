6f72bf3e9e9ee837d21c158bb1a82b6b7d45d89d
Mauricio Rivera <kyonjedi@gmail.com>
2019-01-22 12:04:10 -0300

    first commit

--- client/app/components/Dashboard/Dashboard.js
+++ client/app/components/Dashboard/Dashboard.js
@@ -3,12 +3,35 @@ import ReactTable from 'react-table';
 
 class Dashboard extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hits: [],
+    };
+  }
+
+  componentDidMount(){
+    fetch('/api/admin/licencias/get')
+      .then(response => response.json())
+      .then(data => this.setState({ hits: data.hits }));
+  }
+
   render() {
+    const {hits} = this.state;
     return (
       <div>
+        <ul>
+          {hits.map(hit =>
+            <li key={hit.objectID}>
+              <a href={hit.url}>{hit.title}</a>
+            </li>
+          )}
+        </ul>
 
       </div>
     );
   }
 
 }
+export default Dashboard;
