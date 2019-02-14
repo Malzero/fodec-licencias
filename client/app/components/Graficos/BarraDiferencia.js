93f47efe4bc5f29105424dda0f3268f150de9a65
José <effektopodcast@gmail.com>
2019-02-14 11:08:08 -0300

    14 febrero

--- client/app/components/Graficos/BarraDiferencia.js
+++ client/app/components/Graficos/BarraDiferencia.js
@@ -37,7 +37,15 @@ class BarraDiferencia extends Component {
   }
 
 
+
+
+
+
+
+
+
   renderEditable(cellInfo) {
+
     return (
       <div
         style={{ backgroundColor: "#fafafa" }}
@@ -70,73 +78,14 @@ class BarraDiferencia extends Component {
     //console.log(temp);
     const data = this.state.licencias;
 
-    const columns =
-      [{
-        Header: 'id_licencia',
-        accessor: 'id_licencia' // String-based value accessors!
-      },
-        {
-          Header: 'rut',
-          accessor: 'rut' // String-based value accessors!
-        },
-        {
-          Header: 'nombre',
-          accessor: 'nombre' // String-based value accessors!
-        },
-        {
-          Header: 'colegio',
-          accessor: 'colegio' // String-based value accessors!
-        },
-        {
-          Header: 'dias',
-          accessor: 'dias' // String-based value accessors!
-        },
-        {
-          Header: 'fecha inicio',
-          accessor: 'fecha_inicio' // String-based value accessors!
-        },
-        {
-          Header: 'fecha termino',
-          accessor: 'fecha_termino' // String-based value accessors!
-        },
-        {
-          Header: 'dias pago',
-          accessor: 'dias_pago' // String-based value accessors!
-        },
-        {
-          Header: 'mes pago',
-          accessor: 'mes_pago' // String-based value accessors!
-        },
-        {
-          Header: 'sis salud',
-          accessor: 'sis_salud' // String-based value accessors!
-        },
-        {
-          Header: 'pago fodec',
-          accessor: 'pago_fodec' // String-based value accessors!
-        },
-        {
-          Header: 'estado',
-          accessor: 'estado', // String-based value accessors!
-          //Cell: this.renderEditable
-        },
-        {
-          Header: 'recuperado',
-          accessor: 'recuperado' // String-based value accessors!
-        },
-        {
-          Header: 'perdida',
-          accessor: 'perdida' // String-based value accessors!
-        },
-        {
-          Header: 'Opciones',
-        },
-      ];
+
+console.log(data[0]);
+
 
     const data2 = [
       {
         "name": "Cruz Blanca",
-        "Devolución Isapre": 4000,
+        "Devolución Isapre": 1234,
         "Pérdida FODEC": 2400,
         "amt": 2400
       },
